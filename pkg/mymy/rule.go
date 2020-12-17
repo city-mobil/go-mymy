ab0d3b6567ba22f5c635db1f4c9b8eaa3ea5b6e2
Pavel Parshin <p.parshin@city-mobil.ru>
2020-12-17 13:50:03 +0300

    Added helper to find column by name

--- pkg/mymy/rule.go
+++ pkg/mymy/rule.go
@@ -1,12 +1,35 @@
 package mymy
 
-import "strings"
+import (
+	"errors"
+	"strings"
+)
+
+var (
+	ErrColumnNotFound = errors.New("column not found")
+)
 
 type SourceInfo struct {
 	Schema string
 	Table  string
 	PKs    []Column
 	Cols   []Column
+}
+
+func (info SourceInfo) FindColumnByName(name string) (Column, error) {
+	for _, col := range info.PKs {
+		if col.Name == name {
+			return col, nil
+		}
+	}
+
+	for _, col := range info.Cols {
+		if col.Name == name {
+			return col, nil
+		}
+	}
+
+	return Column{}, ErrColumnNotFound
 }
 
 type Rule struct {
